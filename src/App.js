0d70af38e9d94224ceacda9aac170a76ceee5fa2
Grant Johnson <grantlj@comcast.net>
2022-06-19 06:27:57 -0400

    Pages and page toggle added

--- src/App.js
+++ src/App.js
@@ -1,2 +1,65 @@
+import React from 'react'
+import Question from './components/Question'
+
 export default function App() {
+/*
+API: https://opentdb.com/api.php?
+	amount=10 (or 1-50)
+	&category=9 (9-32 or not included)
+	&difficulty=easy (or medium/hard or not included)
+	&type=multiple (or boolean for true/false or not specified)
+*/
+	const [newGame, setNewGame] = React.useState(true)
+
+	function toggleNewGame(){
+		setNewGame(prevState => !prevState)
+	}
+
+	React.useEffect(() => {
+		fetch("https://opentdb.com/api.php?amount=1")
+			.then(res => res.json())
+			.then(data => console.log(data.results[0]))
+	}, [])
+
+	function generateNewQuestions(){
+		const questionsArray = []
+
+		for(let i = 0; i < 5; i++){
+			questionsArray.push(<Question />)
+		}
+
+		return questionsArray
+	}
+
+	const [allQuestions, setAllQuestions] = React.useState(generateNewQuestions())
+
+
+	return (
+		<div className="main-container">
+			{newGame && <div className="start-screen">
+				<h1>Quizzical</h1>
+				<form>
+					<input type="text" name="numQuestions"/>
+					<select>
+						<option value="9">General Knowledge</option>
+					</select>
+					<select>
+						<option value="easy">Easy</option>
+						<option value="medium">Medium</option>
+						<option value="hard">Hard</option>
+					</select>
+					<select>
+						<option value="multiple">Multiple Choice</option>
+						<option value="boolean">True / False</option>
+					</select>
+				</form>
+				<p className="start-description">Random description text blah blah blah</p>
+				<button className="start-button" onClick={toggleNewGame}>Start Quiz</button>
+			</div>}
+			{!newGame && <form className="play-screen">
+				{allQuestions}
+				<button onClick={toggleNewGame}>Check Answers</button>
+			</form>}
+		</div>
+	)
 }
---
+++ src/components/Question.js
@@ -0,0 +1,13 @@
+export default function Question() {
+    return (
+        <div className="question">
+            <label>Question</label>
+            <div className="question-choices">
+                <button>Option</button>
+                <button>Option</button>
+                <button>Option</button>
+                <button>Option</button>
+            </div>
+        </div>
+    )
+} 
